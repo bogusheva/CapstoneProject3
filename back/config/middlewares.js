d402337bc7d7e41c7dbc07971d7ad255cb14e046
bogusheva <110731443+bogusheva@users.noreply.github.com>
2023-03-26 22:36:42 +0300

    add settings to back middlewares

--- back/config/middlewares.js
+++ back/config/middlewares.js
@@ -1,12 +1,19 @@
 module.exports = [
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::errors",
+  "strapi::security",
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
+
+module.exports.settings = {
+  cors: {
+    enabled: true,
+    origin: ["https://the-teath-avenue.netlify.app", "http://localhost:3000"],
+  },
+};
